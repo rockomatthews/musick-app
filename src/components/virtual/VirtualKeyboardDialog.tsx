1f38ee6c9c8d8a8258b346c1ed2918e19fa3fcca
rockomatthews <rob@robmatthe.ws>
2025-12-30 17:28:41 -0700

    make stems work 3

--- src/components/virtual/VirtualKeyboardDialog.tsx
+++ src/components/virtual/VirtualKeyboardDialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { Box, Dialog, DialogContent, DialogTitle, IconButton, Stack, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import Grid from "@mui/material/Grid";
 import CloseIcon from "@mui/icons-material/Close";
 import * as Tone from "tone";
 import { getAudioEngine } from "@/audio/engine";
